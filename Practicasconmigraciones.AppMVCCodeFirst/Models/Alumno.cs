660676bc3903e526ba48a42d70cce05945a32487
Ivan <User@WIN-JBH2C2BUVPP>
2025-03-07 15:38:38 -0600

    Actualizacion de clases y DbContext

---
+++ Practicasconmigraciones.AppMVCCodeFirst/Models/Alumno.cs
@@ -0,0 +1,13 @@
+﻿namespace Practicasconmigraciones.AppMVCCodeFirst.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Código { get; set; }
+        public string Dirección { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public short YearEstudio { get; set; }
+    }
+}
---
+++ Practicasconmigraciones.AppMVCCodeFirst/Models/MiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Practicasconmigraciones.AppMVCCodeFirst.Models
+{
+    public class MiDbContext: DbContext
+    {
+        public MiDbContext(DbContextOptions<MiDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumnoss { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+
+
+    }
+}
